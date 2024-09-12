677ac384558d95fbdc6ed4f6bad62e93bb74e219
Gabriel Mazzini <100140182+gabrielmazzini@users.noreply.github.com>
2024-09-12 14:44:19 -0300

    finalizacao projeto base

--- src/infra/database/firebase/firebase-config.ts
+++ src/infra/database/firebase/firebase-config.ts
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable max-len */
-import admin from "firebase-admin";
+var admin = require("firebase-admin");
 
-const serviceAccount = require("");
+const serviceAccount = require("./teste-esqueleton-backend-firebase-adminsdk-f1lva-932aee9a9f.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://crud-mais-solid-default-rtdb.firebaseio.com",
+  credential: admin.credential.cert(serviceAccount)
 });
 
-export const usersCollection = "users";
+export const usersCollection = "/users";
